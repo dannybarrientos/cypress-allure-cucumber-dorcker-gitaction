92267417aa3ad574567bdd3a628a4b50d89cd5ec
Danny Andres Barrientos Agudelo <dannybarrientosagudelo@gmail.com>
2024-09-15 22:34:28 -0500

    Folder Report

--- cypress/utils/IteractionsElement.js
+++
@@ -1,6 +0,0 @@
-Cypress.Commands.add('elementIsVisible', (element) => {
-    cy.get(element).should('be.visible');
-});
-Cypress.Commands.add('clickElement', (element) => {
-    cy.get(element).click();
-});
